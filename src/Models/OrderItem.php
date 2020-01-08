3123f9da9d56eb0ccf3cdc3bed4f4fd53ce08146
Zachary Craig <zack@zcraig.me>
2020-01-08 12:20:21 -0500

    Refactor attributes array into properties

--- src/Models/OrderItem.php
+++ src/Models/OrderItem.php
@@ -6,26 +6,95 @@ namespace MichaelB\ShipStation\Models;
 
 class OrderItem extends BaseModel
 {
-    protected $attributes = [
-        'orderItemId',
-        'lineItemKey',
-        'sku',
-        'name',
-        'imageUrl',
-        'weight',
-        'quantity',
-        'unitPrice',
-        'taxAmount',
-        'shippingAmount',
-        'warehouseLocation',
-        'options',
-        'productId',
-        'fulfillmentSku',
-        'adjustment',
-        'upc',
-        'createDate',
-        'modifyDate'
-    ];
+    /**
+     * @var int
+     */
+    protected $orderItemId;
+
+    /**
+     * @var string
+     */
+    protected $lineItemKey;
+
+    /**
+     * @var string
+     */
+    protected $sku;
+
+    /**
+     * @var string
+     */
+    protected $name;
+
+    /**
+     * @var string
+     */
+    protected $imageUrl;
+
+    /**
+     * @var Weight
+     */
+    protected $weight;
+
+    /**
+     * @var int
+     */
+    protected $quantity;
+
+    /**
+     * @var int
+     */
+    protected $unitPrice;
+
+    /**
+     * @var int
+     */
+    protected $taxAmount;
+
+    /**
+     * @var int
+     */
+    protected $shippingAmount;
+
+    /**
+     * @var string
+     */
+    protected $warehouseLocation;
+
+    /**
+     * @var ItemOption[]
+     */
+    protected $options;
+
+    /**
+     * @var int
+     */
+    protected $productId;
+
+    /**
+     * @var string
+     */
+    protected $fulfillmentSku;
+
+    /**
+     * @var boolean
+     */
+    protected $adjustment;
+
+    /**
+     * @var string
+     */
+    protected $upc;
+
+    /**
+     * @var string
+     */
+    protected $createDate;
+
+    /**
+     * @var string
+     */
+    protected $modifyDate;
 
     protected function setWeightAttribute($value)
     {
